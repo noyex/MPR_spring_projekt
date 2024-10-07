1b514025830abca04335e449f27b64a350386ebd
noyex <153921909+noyex@users.noreply.github.com>
2024-10-07 11:14:05 +0200

    Utworzenie projektu

---
+++ src/test/java/pl/edu/pjatk/LAB_2/Lab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pjatk.LAB_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
